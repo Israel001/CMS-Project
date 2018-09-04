4fc8c0f5bd8e4dd768b12e2ba599058dd45ca4a9
Israel Obanijesu <32342912+Israel001@users.noreply.github.com>
2018-09-04 17:27:26 +0700

    updated config.php

--- classes/config.php
+++ classes/config.php
@@ -1,10 +1,10 @@
 <?php
 
 class Config{
-    const SMTP_HOST = 'smtp.gmail.com';
-    const SMTP_PORT = 465;
-    const SMTP_USER = 'israeloogch17619@fpt.edu.vn';
-    const SMTP_PASS = '08163959742';
+    const SMTP_HOST = 'YOUR SMTP HOST';
+    const SMTP_PORT = YOUR SMTP PORT;
+    const SMTP_USER = 'YOUR EMAIL ADDRESS';
+    const SMTP_PASS = 'YOUR PASSWORD';
 }
 
 ?>
